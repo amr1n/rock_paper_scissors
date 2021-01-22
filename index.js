de4378044bdcf1316e36ce899cf995c6fe06ebc0
amr1n <52592047+amr1n@users.noreply.github.com>
2021-01-22 08:14:06 +0600

    added functionality to display the running scor and final winner on browser window.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 //  results and scores output
-let results = document.querySelector('.results');
+let results = document.querySelector('.resultsTxt');
 let scores = document.querySelector('.scores');
 
 //computer selection. 
@@ -23,75 +23,62 @@ function computerPlay() {
 // playing a single round
 function playRound(playerSelection, computerSelection) {
   
-  if ( playerSelection === "rock" && computerSelection === "scissors") {
-
+  if ( playerSelection === "rock" && computerSelection === "scissors") { 
     playerScore++;
-    console.log("You won. Rock beats Scissors");
+    results.textContent = "You won. Rock beats Scissors";
 
-  }else if (playerSelection === "scissors" && computerSelection === "rock") {
-
+  }else if (playerSelection === "scissors" && computerSelection === "rock") { 
     computerScore++;
-    console.log( "You lost. Rock beats Scissors");
+    results.textContent = "You lost. Rock beats Scissors";
 
-  }else if (playerSelection === "paper" && computerSelection === "scissors") {
-
+  }else if (playerSelection === "paper" && computerSelection === "scissors") { 
     computerScore++;
-    console.log( "You lost. Scissors beats Paper.");
+    results.textContent = "You lost. Scissors beats Paper.";
 
-  }else if (playerSelection === "scissors" && computerSelection === "paper") {
-
+  }else if (playerSelection === "scissors" && computerSelection === "paper") { 
     playerScore++;
-    console.log( "You won. Scissors beats Paper.");
+    results.textContent = "You won. Scissors beats Paper.";
 
-  }else if (playerSelection === "rock" && computerSelection === "paper") {
-
+  }else if (playerSelection === "rock" && computerSelection === "paper") { 
     computerScore++;
-    console.log( "You lost. Paper beats Rock.");
+    results.textContent = "You lost. Paper beats Rock.";
 
-  }else if (playerSelection === "paper" && computerSelection === "rock") {
-
+  }else if (playerSelection === "paper" && computerSelection === "rock") { 
     playerScore++;
-    console.log( "You won. Paper beats Rock.");
+    results.textContent = "You won. Paper beats Rock.";
 
   }else{
-    console.log( "Its a tie. Play again.");
+    results.textContent = "Its a tie. Play again.";
   }
 }
 
 
 function game() {
-  computerSelection = computerPlay();
-  console.log(playerSelection, computerSelection);
+  computerSelection = computerPlay(); 
   playRound(playerSelection,computerSelection);
-
-  console.log(playerScore, computerScore)
-  if (playerScore === 5 &&  computerScore < 5) {
-  console.log("You are the winner of this game. congrats.");
-  console.log(playerScore, computerScore)
-
-  }else if (playerScore < 5 && computerScore === 5){
-  console.log("You lose. computer is the winner.");
-  console.log(playerScore , computerScore)
-  }
+  scores.textContent = `${playerScore} ${computerScore}`; 
 }
 
 
 // dom
 let buttons = document.querySelectorAll('button');
+let playerSelectionBtn;
 for (i = 0; i < buttons.length; i++) {
-    let btn = buttons[i];
+    playerSelectionBtn = buttons[i];
     function callback() {
-      if (btn.className === "rock") {
+      if (playerSelectionBtn.className === "rock") {
         playerSelection = "rock";
-      }else if (btn.className === "scissors"){
+      }else if (playerSelectionBtn.className === "scissors"){
         playerSelection = "scissors";
-      }else {
+      }else if (playerSelectionBtn.className === "paper"){
         playerSelection = "paper";
       }
 
       game();
     }
 
-    btn.addEventListener('click', callback);
+    playerSelectionBtn.addEventListener('click', callback);
 
 }
+
+
